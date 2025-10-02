beff10a8d9f6c787ba79faf2c5f4a932fbc2663d
Inpyo <ypd02363@naver.com>
2025-10-02 19:05:20 +0200

    [front] encapsulating API key and update API request logics (from Angular to express.js)

--- src/app/components/station-board/station-board.ts
+++ src/app/components/station-board/station-board.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { LocationDetail } from '@app/models/data.model';
+import { LocationDetail, LocationDetailMap } from '@app/models/data.model';
 import { RemainingTimePipe } from '@app/pipes/remaining-time-pipe';
 import { map, Observable, startWith, Subject, takeUntil, timer } from 'rxjs';
 
@@ -16,7 +16,7 @@ export class StationBoard implements OnInit, OnDestroy{
   currentTime$: Observable<Date> = timer(0, 1000).pipe(map(()=> new Date()));
   autoSearchControl = new FormControl(false);
 
-  @Input() stationDetails: LocationDetail[] | null = [];
+  @Input() stationDetails: LocationDetailMap[] | null = [];
   @Output() autoSearchToggle = new EventEmitter<boolean>();
 
   private destroy$ = new Subject<void> ();
@@ -40,8 +40,8 @@ export class StationBoard implements OnInit, OnDestroy{
   getRemainingTimeClass(etTime: string, now:Date): string{
     const etTimeC = new Date(etTime);
     const diffInMinutes = Math.floor((etTimeC.getTime() - now.getTime())/60000)
-    if (diffInMinutes <= 0) return 'departed';
-    if (diffInMinutes < 1) return 'imminent';
+    if (diffInMinutes < 0) return 'departed';
+    if (diffInMinutes <= 1) return 'imminent';
     return '';
   }
 
