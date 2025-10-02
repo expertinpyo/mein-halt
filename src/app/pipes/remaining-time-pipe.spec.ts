c2960d4395aa785abd9f86dfa1727ce5a554666f
Inpyo <ypd02363@naver.com>
2025-10-02 19:18:46 +0200

    [frontend] adjust : directory revision

--- src/app/pipes/remaining-time-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RemainingTimePipe } from './remaining-time-pipe';
-
-describe('RemainingTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new RemainingTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
