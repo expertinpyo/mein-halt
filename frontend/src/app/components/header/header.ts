871c3bdd7c8f329bbbf064f0bf656c1de893805f
Inpyo <ypd02363@naver.com>
2025-10-03 17:59:53 +0200

    [frontend] update sbb header - redirect function add

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -8,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Header {
 
+  onLogoClick(): void {
+    const userAgent = navigator.userAgent.toLocaleLowerCase()
+    const isMobile = /android|iphone|ipad|ipod/i.test(userAgent)
+    
+    if(isMobile){
+      window.location.href = 'sbbmobile://'
+      
+      setTimeout(() => {
+        if(/android/i.test(userAgent)){
+          window.location.href = 'https://play.google.com/store/apps/details?id=ch.sbb.mobile.android.b2c';
+        } else if (/iphone|ipad|ipod/i.test(userAgent)) {
+          window.location.href = 'https://apps.apple.com/ch/app/sbb-mobile/id294855237';
+        }
+      }, 1200);
+    } else {
+      window.open('https://www.sbb.ch', '_blank')
+    }
+  }
 }
