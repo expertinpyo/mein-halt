af427a0cc6bb2f2bcf2dd79d8c86f6659167b9a2
Inpyo <ypd02363@naver.com>
2025-10-03 18:09:05 +0200

    [frontend] update mobile redirection modification

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -10,20 +10,47 @@ export class Header {
 
   onLogoClick(): void {
     const userAgent = navigator.userAgent.toLocaleLowerCase()
-    const isMobile = /android|iphone|ipad|ipod/i.test(userAgent)
     
-    if(isMobile){
+    
+    const isAndroid = /android/.test(userAgent);
+    const isIOS = /iphone|ipad|ipod/.test(userAgent);
+    
+    
+    if(isAndroid){
       window.location.href = 'sbbmobile://'
-      
-      setTimeout(() => {
-        if(/android/i.test(userAgent)){
-          window.location.href = 'https://play.google.com/store/apps/details?id=ch.sbb.mobile.android.b2c';
-        } else if (/iphone|ipad|ipod/i.test(userAgent)) {
-          window.location.href = 'https://apps.apple.com/ch/app/sbb-mobile/id294855237';
-        }
-      }, 1200);
-    } else {
-      window.open('https://www.sbb.ch', '_blank')
+      const storeUrl = encodeURIComponent('https://play.google.com/store/apps/details?id=ch.sbb.mobile.android.b2c')
+      const intent = `intent://#Intent;scheme=sbbmobile;package=ch.sbb.mobile.android.b2c;S.browser_fallback_url=${storeUrl};end`;
+      window.location.href = intent;
+      return;
     }
+    if (isIOS) {
+      const appStoreUrl = 'https://apps.apple.com/ch/app/sbb-mobile/id294855237';
+
+      let fallbackTimer: any;
+
+      const cancelFallback = () => {
+        if (fallbackTimer) clearTimeout(fallbackTimer);
+        window.removeEventListener('pagehide', cancelFallback);
+        window.removeEventListener('blur', cancelFallback);
+        document.removeEventListener('visibilitychange', onVisChange);
+      };
+      const onVisChange = () => {
+        if (document.hidden) cancelFallback();
+      };
+
+      window.addEventListener('pagehide', cancelFallback);
+      window.addEventListener('blur', cancelFallback);
+      document.addEventListener('visibilitychange', onVisChange);
+
+      window.location.href = 'sbbmobile://';
+
+      fallbackTimer = setTimeout(() => {
+        window.location.href = appStoreUrl;
+      }, 1200);
+
+      return;
+    }
+
+    window.open('https://www.sbb.ch', '_blank');
   }
 }
